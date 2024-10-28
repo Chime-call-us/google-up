99053f753fbf6c462a94847e6452c8f859994efb
App-support-service <145561439+Chime-call-us@users.noreply.github.com>
2024-10-29 01:04:40 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,2 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1‪‪(805) 246-7061 ‬ to unlock it!';defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1‪‪(805) 246-7061‬
-‬ to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1‪(805) 246-7061‬ to unlock it!';
